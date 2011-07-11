643fa90b1532a9f48ae55b10cac032708945a26c
Binlong Li <herbert19lee@gmail.com>
2011-07-11 04:16:08 -0400

    Lines Counting for all possible projects. Need to install Numpy package

--- LinesCounting/LinesCounting_folder.py
+++ LinesCounting/LinesCounting_folder.py
@@ -1,34 +1,39 @@
 #!/usr/bin/python
-
 import commands
 import os
 import datetime
 
-#os.chdir("/git/project/dir")
-os.chdir("C:\CProjects\Kinect_OpenNI\RGBDemo-0.5.0-Source\RGBDemo-0.5.0-Source\mysuperdemo")
-##os.chdir("C:\CProjects\LearningBoost")
-##os.chdir("C:\CProjects\LearningPython")
 
-author = "herbert19lee@gmail.com"
+def folderLines(folder):
+
+
+#os.chdir("/git/project/dir")
+#folder = "C:\CProjects\Kinect_OpenNI\RGBDemo-0.5.0-Source\RGBDemo-0.5.0-Source\mysuperdemo"
+#folder = "C:\CProjects\LearningBoost"
+#folder = "C:\CProjects\LearningPython"
+    os.chdir(folder)
+
+    author = "herbert19lee@gmail.com"
 
 ##hist = os.popen("git log --shortstat --reverse --pretty=medium --since=07/06/2011 --author=\"" + author + "\" --no-merges").read()
-d = datetime.date.today()
-date = d.isoformat()
+    d = datetime.date.today()
+    date = d.isoformat()
 ##time = "01:00am"
-time = "yesterday"
-commandline = "git log --shortstat --reverse --pretty=oneline --since \"" + time + "\" --author=herbert19lee@gmail.com --no-merges"
+    time = "yesterday"
+    commandline = "git log --shortstat --reverse --pretty=oneline --since \"" + time + "\" --author=herbert19lee@gmail.com --no-merges"
 
 ##print commandline
-hist = os.popen(commandline).read()
-print hist
+    hist = os.popen(commandline).read()
+    print hist
 
-hist = hist.split("\n")
-totalins = 0
+    hist = hist.split("\n")
+    totalins = 0
 
-for line in hist:
-    if line.startswith(' '):
-        ins =  line.split(",")[1]
-        totalins = totalins + int(ins.split(" ")[1])
+    for line in hist:
+        if line.startswith(' '):
+            ins =  line.split(",")[1]
+            totalins = totalins + int(ins.split(" ")[1])
 
 
-print "Since " + date + "-" +  time + " you (" + author + ") wrote " + str(totalins) + " lines of code!"
+            print "Since " + date + "-" +  time + " you (" + author + ") wrote " + str(totalins) + " lines of code!"
+            return (totalins)
