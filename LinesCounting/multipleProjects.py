837e221dc2c3889e8c2668f91c09029dff110667
Binlong Li <herbert19lee@gmail.com>
2011-07-31 23:49:08 -0400

    LinesCounting project, python, can work on both PC and Mac

--- LinesCounting/multipleProjects.py
+++ LinesCounting/multipleProjects.py
@@ -1,11 +1,18 @@
 import sys
-sys.path.insert(0, 'C:\\CProjects\\LearningPython\\LinesCounting')
+if sys.platform.startswith('win32'):
+    sys.path.insert(0, 'C:\\CProjects\\LearningPython\\LinesCounting')
+elif sys.platform.startswith('darwin'):
+    sys.path.insert(0, '/Users/herbert19lee/Develop/DevelopPython/LearningPython')
+                    
 
 import LinesCounting_folder
 import numpy
 
-folderNameS = ["C:\CProjects\Kinect_OpenNI\RGBDemo-0.5.0-Source\RGBDemo-0.5.0-Source\mysuperdemo", 
+if sys.platform.startswith('win32'):
+    folderNameS = ["C:\CProjects\Kinect_OpenNI\RGBDemo-0.5.0-Source\RGBDemo-0.5.0-Source\mysuperdemo", 
                "C:\CProjects\LearningBoost", "C:\CProjects\LearningPython"]
+elif sys.platform.startswith('darwin'):
+    folderNameS = ["/Users/herbert19lee/Develop/DevelopPython"];
 
 linesTotalProject = 0
 linesOneProjectArray = [];
