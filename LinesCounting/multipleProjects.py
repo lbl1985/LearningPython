c66974d4d832411baab35e9a02ad7990483a6c7d
Binlong Li <herbert19lee@gmail.com>
2011-08-14 22:05:39 -0400

    add more supporting projects

--- LinesCounting/multipleProjects.py
+++ LinesCounting/multipleProjects.py
@@ -12,7 +12,9 @@ if sys.platform.startswith('win32'):
     folderNameS = ["C:\CProjects\Kinect_OpenNI\RGBDemo-0.5.0-Source\RGBDemo-0.5.0-Source\mysuperdemo", 
                "C:\CProjects\LearningBoost", "C:\CProjects\LearningPython"]
 elif sys.platform.startswith('darwin'):
-    folderNameS = ["/Users/herbert19lee/Develop/DevelopPython/LearningPython"]
+    folderNameS = ["/Users/herbert19lee/Develop/DevelopPython/LearningPython",
+                   "/Users/herbert19lee/Documents/MATLAB/work/celltrack",
+                   "/Users/herbert19lee/Documents/MATLAB/work/HFeature"]
 
 linesTotalProject = 0
 linesOneProjectArray = [];
